c949524b8f4223e3a1a5f6ba8cda5dd3cc40814a
hemul <hemul@pochtamt.ru>
2012-12-15 14:02:09 +0300

    Added new packages (Interfaces, Classes). Added Classes test case suit.

---
+++ src/test/java/net/hemul/ipcam/test/IPCamClassesTests.java
@@ -0,0 +1,14 @@
+package net.hemul.ipcam.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class IPCamClassesTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
