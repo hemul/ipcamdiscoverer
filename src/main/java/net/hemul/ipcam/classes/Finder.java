2539c8228e2adbd1852992ff984fc50d02c011c3
hemul <hemul@pochtamt.ru>
2012-12-16 21:43:41 +0300

    Finder: Exception handling has been changed.

--- src/main/java/net/hemul/ipcam/classes/Finder.java
+++ src/main/java/net/hemul/ipcam/classes/Finder.java
@@ -9,7 +9,6 @@ import com.ms.wsdiscovery.WsDiscoveryFinder;
 import com.ms.wsdiscovery.exception.WsDiscoveryException;
 import com.ms.wsdiscovery.network.exception.WsDiscoveryNetworkException;
 import com.ms.wsdiscovery.servicedirectory.WsDiscoveryService;
-import com.ms.wsdiscovery.servicedirectory.exception.WsDiscoveryServiceDirectoryException;
 import com.ms.wsdiscovery.servicedirectory.interfaces.IWsDiscoveryServiceCollection;
 
 public class Finder {
@@ -19,9 +18,6 @@ public class Finder {
 	private List<String> foundServices;
 
 	public Finder() {
-
-		// Create new finder instance.
-//		System.out.println("Creating new finder-instance...");
 
 		foundServices = new ArrayList<>();
 
@@ -37,16 +33,8 @@ public class Finder {
 
 	public void probe() {
 
-//		System.out.println("Searching for all services (2 sec).");
-
 		try {
 			result = finder.findAll(2000);
-		} catch (WsDiscoveryServiceDirectoryException | InterruptedException e) {
-			e.printStackTrace();
-		}
-
-		// Stop finder
-		try {
 			finder.done();
 		} catch (InterruptedException | WsDiscoveryException e) {
 			e.printStackTrace();
@@ -54,17 +42,12 @@ public class Finder {
 	}
 
 	public List<String> getResultList() {
-		// Display the results.
-		// System.out.println("** Discovered services: **");
 
 		for (WsDiscoveryService service : result) {
-			// Print service info
 			foundServices.add(service.getXAddrs().get(0));
-
-			// System.out.println("---");
 		}
+
 		return foundServices;
 
 	}
-
 }
