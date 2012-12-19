aa5c533b83cc74c28614f057e2fadf83c2d3db88
hemul <hemul@192.168.1.7>
2012-12-19 19:43:25 +0300

    MySQLDBStorage and MySQLDBStorageTest classes changed

--- src/main/java/net/hemul/ipcam/classes/MySQLDBStorage.java
+++ src/main/java/net/hemul/ipcam/classes/MySQLDBStorage.java
@@ -5,14 +5,29 @@ import java.util.List;
 import net.hemul.ipcam.interfaces.*;
 
 public class MySQLDBStorage implements DataStorage {
+	
+	private String DB_SID;
+	private String user;
+	private String password;
 
-	public MySQLDBStorage(String DBSID, String user, String password) {
+	public MySQLDBStorage(String DB_SID, String user, String password) {
+		
+		this.DB_SID = DB_SID;
+		this.user = user;
+		this.password = password;
+		
+		createDBConnection();
+	}
 
+	private void createDBConnection() {
+		
 	}
 
 	@Override
 	public boolean storeData(List<String> dataToStore) {
-		return true;
+		boolean successfully;
+		successfully = true;
+		return successfully;
 	}
 
 }
--- src/test/java/net/hemul/ipcam/test/MySQLDBStorageTest.java
+++ src/test/java/net/hemul/ipcam/test/MySQLDBStorageTest.java
@@ -2,6 +2,9 @@ package net.hemul.ipcam.test;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import net.hemul.ipcam.classes.MySQLDBStorage;
 import net.hemul.ipcam.interfaces.DataStorage;
 
@@ -10,11 +13,30 @@ import org.junit.Test;
 public class MySQLDBStorageTest {
 	
 	private DataStorage dataStorage;
+	
 
 	@Test
 	public void create() {
 		dataStorage = new MySQLDBStorage("localhost:3306\testdb" , "test" , "test");
 		assertNotNull(dataStorage);
+	}
+	
+	@Test
+	public void testStoreMethod() {
+		dataStorage = new MySQLDBStorage("localhost:3306\testdb" , "test" , "test");
+		List<String> listToStore = new ArrayList<>();
+		fillListWithTestData(listToStore);
+		boolean successfully = dataStorage.storeData(listToStore);
+		assertTrue(successfully);
+		
+	}
+
+	private void fillListWithTestData(List<String> listToStore) {
+		listToStore.add("url1");
+		listToStore.add("url2");
+		listToStore.add("url3");
+		listToStore.add("url4");
+		
 	}
 
 }
